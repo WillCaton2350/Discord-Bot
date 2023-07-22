806a4dd1c24ac789431f616f86efd853f405ca9a
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-07-22 14:14:28 -0400

    Add files via upload

---
+++ pytest/Bot.py
@@ -0,0 +1,40 @@
+import responses
+import discord 
+
+
+async def send_message(message, user_message,is_private):
+    try:
+        respo = responses.get_response(
+            user_message)
+        await message.author.send(
+            respo) if is_private else await message.channel.send(
+                respo)
+    except Exception as err:
+        print(err)
+    
+
+def run_Discord_Bot():
+    TOKEN = 'MTEzMjE0ODU3MDE1NDM1Njc0Ng.GI5mVA.rYMQHV6pcLLYwva1oJLjQZH19y6zEh7aEe8whc'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.guild_messages = True
+    client = discord.Client(intents=intents)
+    
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        print(username, channel)
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message,user_message,is_private=False)
+    client.run(TOKEN)
+        
---
+++ pytest/responses.py
@@ -0,0 +1,34 @@
+import random
+import json
+
+def get_response(message: str) -> str:
+    with open("dictionary.json", 'r') as json_file:
+        definitions = json.load(json_file)
+
+    define_phrases = ['define', 'definition of', 'definition for', 'meaning of']
+    
+    definitions = {key.lower(): value for key, value in definitions.items()}
+
+    for phrase in define_phrases:
+        if message.lower().startswith(phrase):
+            word_to_define = message[len(phrase):].strip().lower()
+            if word_to_define in definitions:
+                return definitions[word_to_define]
+
+    if message.lower() == 'roll':
+        return str(random.randint(1, 6))
+
+    if message.lower() == '!help':
+        return '''`Guide:
+- Activation - Define 
+- limited definitions
+- Utilizes the Webster dictionary
+- Type "roll" to roll the dice :)`'''
+
+    greetings = ['hello','helo','hey there', 'hey','hia', 'yo', 'you there?', 'hi']
+    if message.lower() in greetings:
+        return "Hey!" + " Type !help for a Bot Guide"
+    
+
+    print(f"Unhandled message: '{message}'")
+    return "Sorry I didn't understand what you said... Try typing !help"
